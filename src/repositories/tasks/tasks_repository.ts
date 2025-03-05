9e92558d0f4ded948a7dd27555c838aced9db53b
Jasmine Pinheiro <110035728+JasminePinheiro@users.noreply.github.com>
2025-03-05 00:16:18 -0300

    feat: implement abstract repository and MongoDB operations for tasks

---
+++ src/repositories/tasks/tasks_repository.ts
@@ -0,0 +1,30 @@
+import { ITask, Task } from '../../models/TaskModel';
+
+export abstract class AbsTasksRepository {
+    abstract getAllTasks(): Promise<ITask[]>
+    abstract createTask(task: ITask): Promise<ITask>;
+    abstract updateTask(taskId: string, updatedTask: ITask): Promise<ITask>;
+    abstract deleteTask(taskId: string): Promise<boolean>;
+    abstract findTaskById(taskId: string): Promise<ITask | null>;
+}
+
+export class TasksRepository implements AbsTasksRepository {
+    async getAllTasks(): Promise<ITask[]> {
+        return await Task.find()
+    }
+    async createTask(task: ITask): Promise<ITask> {
+        return await Task.create(task)
+    }
+    async updateTask(taskId: string, updatedTask: ITask): Promise<ITask> {
+        return await Task.findByIdAndUpdate(taskId, updatedTask, { new: true })
+    }
+    async deleteTask(taskId: string): Promise<boolean> {
+         await Task.findByIdAndDelete(taskId)
+         const deletedTask = await Task.findById(taskId).select('-__v').lean();
+         return !deletedTask? true : false;
+    }
+    async findTaskById(taskId: string): Promise<ITask | null> {
+        return await Task.findById(taskId)
+    }
+
+}
